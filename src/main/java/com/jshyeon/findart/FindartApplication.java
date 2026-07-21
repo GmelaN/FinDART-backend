3c2fc78ba085b50513f73a59cabc82cf85795813
jshyeon <gmelanals@gmail.com>
2026-07-21 06:39:17 +0000

    redefined project structure

---
+++ src/main/java/com/jshyeon/findart/FindartApplication.java
@@ -0,0 +1,13 @@
+package com.jshyeon.findart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FindartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FindartApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jshyeon/findart/FindartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jshyeon.findart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FindartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
